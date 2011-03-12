288be9f5db63a36bb812d7c49beea6a48a27ce7c
kang <kang@insecure.ws>
2011-03-12 13:28:44 +0100

    easy mutli-kernel release system
    Change-Id: I1c6cac0dcd5b5efa16a58751796659ab8cb3f04e

---
+++ release/doit.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+[[ -d release ]] || {
+	echo "must be in kernel root dir"
+	exit 1;
+}
+
+
+TYPE=$1
+[[ "$TYPE" == '' ]] && TYPE=SGS
+
+REL=CM7_${TYPE}_kernel_kang_$(date +%Y%m%d_%H)_update.zip
+
+cp ./arch/arm/boot/zImage release/ || exit 1
+find . -name "*.ko" -exec cp {} release/system/modules/ \; 2>/dev/null || exit 1
+
+cd release && {
+	zip -q -r ${REL} system zImage META-INF || exit 1
+	sha256sum ${REL} > ${REL}.sha256sum
+	rm -rf ${TYPE} || exit 1
+	mkdir -p ${TYPE} || exit 1
+	mv ${REL}* ${TYPE} || exit 1
+} || exit 1
+
+rm system/modules/*
+echo ${REL}
+exit 0
