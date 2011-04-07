c06f6509d10ebe046323ea3ba1f2907a711bdfcd
Matt B <jackdachef@gmail.com>
2011-04-08 01:10:59 +0200

    add cappy packaging script

---
+++ release/doit_cappy.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+
+[[ -d release ]] || {
+	echo "must be in kernel root dir"
+	exit 1;
+}
+
+
+TYPE=$1
+[[ "$TYPE" == '' ]] && TYPE=CAPTIVATE
+
+REL=CM7_${TYPE}_platypus-kernel_$(date +%Y%m%d_%H)_update.zip
+
+cp ./arch/arm/boot/zImage release/ || exit 1
+rm -r release/system 2> /dev/null
+mkdir  -p release/system/modules || exit 1
+find . -name "*.ko" -exec cp {} release/system/modules/ \; 2>/dev/null || exit 1
+cp -r release/lib release/system || exit 1
+
+cd release && {
+	zip -q -r ${REL} system zImage META-INF || exit 1
+	sha256sum ${REL} > ${REL}.sha256sum
+	rm -rf ${TYPE} || exit 1
+	mkdir -p ${TYPE} || exit 1
+	mv ${REL}* ${TYPE} || exit 1
+} || exit 1
+
+rm -r system
+echo ${REL}
+exit 0
