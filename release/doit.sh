af53634b2f651fe3bbce181fef49f415d98bfb62
Matt B <jackdachef@gmail.com>
2011-04-30 03:24:48 +0200

    update module path to /system/lib/modules

--- release/doit.sh
+++ release/doit.sh
@@ -13,8 +13,8 @@ REL=CM7_${TYPE}_platypus-kernel_$(date +%Y%m%d_%H)_update.zip
 
 cp ./arch/arm/boot/zImage release/ || exit 1
 rm -r release/system 2> /dev/null
-mkdir  -p release/system/modules || exit 1
-find . -name "*.ko" -exec cp {} release/system/modules/ \; 2>/dev/null || exit 1
+mkdir  -p release/system/lib/modules || exit 1
+find . -name "*.ko" -exec cp {} release/system/lib/modules/ \; 2>/dev/null || exit 1
 cp -r release/lib release/system || exit 1
 
 cd release && {
