e9d848db6374b7bdc5ac4af3f121104de418bf01
Dylan <dylan.taber@gmail.com>
2011-05-08 01:53:47 +0200

    Fix build. forgot to add [index]

--- arch/arm/mach-s5pv210/cpu-freq.c
+++ arch/arm/mach-s5pv210/cpu-freq.c
@@ -473,7 +473,7 @@ static int s5pv210_cpufreq_target(struct cpufreq_policy *policy,
 		goto out;
 
 	if(unlikely(exp_UV_mV[index] < -50)) 
-		exp_UV_mV = -50;
+		exp_UV_mV[index] = -50;
 		
 	arm_volt = (dvs_conf[index].arm_volt - (exp_UV_mV[index]*1000));
 
@@ -665,7 +665,7 @@ static int s5pv210_cpufreq_target(struct cpufreq_policy *policy,
 			"cpufreq: Performance changed[L%d]\n", index);
 	
 	if(unlikely(exp_UV_mV[index] < -50)) 
-		exp_UV_mV = -50;
+		exp_UV_mV[index] = -50;
 
 	previous_arm_volt = (dvs_conf[index].arm_volt - (exp_UV_mV[index] * 1000));
 	//freq_uv_table[index][2] = (int) previous_arm_volt / 1000;
@@ -713,7 +713,7 @@ static int s5pv210_cpufreq_resume(struct cpufreq_policy *policy)
 			sizeof(struct s3c_freq));
 			
 	if(unlikely(exp_UV_mV[index] < -50)) 
-		exp_UV_mV = -50;
+		exp_UV_mV[index] = -50;
 
 	previous_arm_volt = (dvs_conf[level].arm_volt - (exp_UV_mV[level]*1000));
 
@@ -785,7 +785,7 @@ static int __init s5pv210_cpufreq_driver_init(struct cpufreq_policy *policy)
 			sizeof(struct s3c_freq));
 			
 	if(unlikely(exp_UV_mV[index] < -50)) 
-		exp_UV_mV = -50;
+		exp_UV_mV[index] = -50;
 
 	previous_arm_volt = (dvs_conf[level].arm_volt - (exp_UV_mV[level]*1000));
 
