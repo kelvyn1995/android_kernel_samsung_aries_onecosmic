321503f61661385bb5ddb135ca8fc6f89c97555d
Matt B <jackdachef@gmail.com>
2011-06-24 05:21:31 +0200

    Revert "mm-idleprio_prio-1"
    This reverts commit db993ef7ed4b7fb4fe99a8e0ca0a5ebd2a9b8b9e.

--- mm/vmscan.c
+++ mm/vmscan.c
@@ -1787,8 +1787,6 @@ static inline int effective_sc_prio(struct task_struct *p)
 	if (likely(p->mm)) {
 		if (rt_task(p))
 			return -20;
-		if (p->policy == SCHED_IDLEPRIO)
-			return 19;
 		return task_nice(p);
 	}
 	return 0;
