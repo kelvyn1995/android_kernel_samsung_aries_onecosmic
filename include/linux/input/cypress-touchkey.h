186f2b99e8efa2ecaadf2c00f49e830d27ddcdbe
Matt B <jackdachef@gmail.com>
2011-07-17 13:09:41 +0200

    some small fixes to BLN

--- include/linux/input/cypress-touchkey.h
+++ include/linux/input/cypress-touchkey.h
@@ -24,8 +24,8 @@
 struct touchkey_platform_data {
 	int keycode_cnt;
 	const int *keycode;
-	void (*touchkey_sleep_onoff) (int);
 	void (*touchkey_onoff) (int);
+	void (*touchkey_sleep_onoff) (int);
 	const char *fw_name;
 	int scl_pin;
 	int sda_pin;
